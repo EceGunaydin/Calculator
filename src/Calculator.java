47142cb56d29842471f59ab5e4c22dc79b232188
Ece Gunaydin <smyyg1998@gmail.com>
2019-11-05 19:47:18 -0500

    second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
     public static double add(double n1 , double n2){
         return n1 + n2 ;
     }
+
+    public static double substract(double n1 , double n2){
+        return n1-n2 ;
+    }
 }
