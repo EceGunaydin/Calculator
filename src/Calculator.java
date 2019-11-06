5417e63ca0c9b771f486fafd21bb206cdbbf59ee
EceGunaydin <57405052+EceGunaydin@users.noreply.github.com>
2019-11-06 16:18:04 -0500

    remote add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,4 +9,6 @@ public class Calculator {
     public static double multiply(double n1 , double n2){
        return n1*n2 ; 
     }
+    public static double divide(double num1 ,double num2){
+        return num1/num2 ;
 }
